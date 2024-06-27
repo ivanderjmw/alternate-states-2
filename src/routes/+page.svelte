79c966221f9e96a715698801afe5292a2ccded2e
Ivander Waskito <ivanderjmw@gmail.com>
2024-06-28 01:28:31 +0800

    Add wordlist check

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,31 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <script lang="ts">
+    import { census } from "./census.js";
+
     let letters = Array(25);
     let loadletters = "A,B,D,E,E,F,G,H,I,J,D,D,X,S,K,D,E,D,C,D,D,S,E,S,D";
     let values = [];
-    let checkWord = "";
     let cacheWords = undefined;
     for (let index = 0; index < letters.length; index++) {
         letters[index] = "";
     }
     let points = 0;
 
-    let check = () => {
-        console.log("Check is called");
-        let pointsC = 0;
-        for (let i = 0; i < letters.length; i++) {
-            pointsC += letters[i] !== "" ? 1 : 0;
-        }
+    let calcPoints = () => {
+        points = 0;
+        Object.keys(census).forEach(k => {
+            if(check(k.toUpperCase()))
+                points += census[k];
+        });
+    }
+        
+
+    let check = (checkWord) => {
+        console.log("Check is called", checkWord);
         values = [];
         let checkLength = checkWord.length;
-        if (checkWord == "" | checkWord == null)
+        if (checkWord == "" || checkWord == null)
             return;
         cacheWords = new Array(checkLength);
         for (let i = 0; i < checkLength; i++)
@@ -35,12 +41,14 @@
             indexes.push(i);
             cacheWords[checkLength - 1][i] = new Set([letters[i]]);
         }
-        nextmoves(checkLength - 1);
-        console.log(cacheWords);
+        nextmoves(checkLength - 1, checkWord);
+        // console.log(cacheWords);
 
-        values = cacheWords[0].map(a => Array.from(a).join(" ")).join("\n");
+        values = cacheWords[0].filter(a => a.size > 0).map(a => Array.from(a).join(" ")).join("\n");
 
-        return pointsC;
+        // console.log(cacheWords, values);
+
+        return cacheWords[0].some(a => a.size > 0);
     }
 
     let kingsmoves = [
@@ -48,7 +56,7 @@
     ]; // kings moves in (dx, dy)
 
 
-    let nextmoves = (count) => {
+    let nextmoves = (count, checkWord) => {
         if (count == 0) return;
 
         for (let y = 0; y < 25; y++) {
@@ -79,22 +87,22 @@
                     cacheWords[count - 1][nextIndex] = new Set();
     
                 var newWords = Array.from(cacheWords[count][prevIndex]).map(a => a + currLetter);
-                console.log("filter", checkWord, newWords);
+                // console.log("filter", checkWord, newWords);
                 newWords = newWords.filter(a => checkWord.startsWith(a));
                 cacheWords[count - 1][nextIndex] = cacheWords[count - 1][nextIndex].union(new Set(newWords));
 
             }
         }
-        nextmoves(count - 1);
+        nextmoves(count - 1, checkWord);
     }
 </script>
 <small>Load letters:</small>
 <input bind:value={loadletters} maxlength="50">
 <button on:click={() => letters = (loadletters + " ").split(',').map(l => l.trim())}>Load</button>
 <div style="display: block;height:2em"></div>
-<small>Check word:</small>
-<input bind:value={checkWord}>
-<button on:click={() => points = check()}>Check</button>
+<!-- <small>Check word:</small>
+<input bind:value={checkWord}> -->
+<button on:click={() => calcPoints()}>Check</button>
 <div style="display: block;height:2em"></div>
 <div>
     {#each Array.from(letters.keys()) as row (row)}
